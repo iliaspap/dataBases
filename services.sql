b78aff9080ab848ea4a57f6ca0cca0b13bae7ae6
Marios Mantalos <marios.mad@gmail.com>
2021-05-25 18:01:07 +0300

    Small mistake.

--- services.sql
+++ services.sql
@@ -1,8 +1,8 @@
-insert into Service(service_description, needs_registration)
-values 	('Room stay', 1), 
-		('Drinks at the bar', 0),
-		('Food and drinks at the restaurant', 0),
-		('Hairdressing', 0), 
-		('Gym', 1), 
-		('Sauna', 1),
-		('Meeting room', 1);
+insert into Service(service_description, needs_registration) values
+('Room stay', 1), 
+('Drinks at the bar', 0),
+('Food and drinks at the restaurant', 0),
+('Hairdressing', 0), 
+('Gym', 1), 
+('Sauna', 1),
+('Meeting room', 1);
