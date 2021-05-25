254e96eedfe72301b36a0e6bcbe8e2633cbbe347
iliaspap <iliaspapanikolaou999@gmail.com>
2021-05-25 22:44:33 +0300

    added insertData + directories

---
+++ insertData.py
@@ -0,0 +1,25 @@
+import mysql.connector
+import csv
+
+def addears(item):
+    return "\'{}\'".format(item)
+
+mydb = mysql.connector.connect(
+    user = "root",
+    passwd = "",
+    database = "Hotel"
+)
+
+mycursor = mydb.cursor()
+
+tablename = input()
+
+with open(f'./csv/{tablename}.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    table = [line for line in csv_reader]
+    for record in table:
+        query = """INSERT INTO {} ({}) VALUES ({})""".format(tablename, ",".join(record.keys()), ",".join(f'"{word}"' for word in record.values()))
+        print(query)
+        # mycursor.execute(query)
+        # mydb.commit()
