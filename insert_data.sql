85e79ecf91809349dd7fec59937dbb5cf02ac709
Marios Mantalos <marios.mad@gmail.com>
2021-05-25 18:58:13 +0300

    Removing a file.

--- insert_data.sql
+++
@@ -1,7 +0,0 @@
-load data local infile '/home/marios/ECE/databases/project/customers.csv' into table Customer 
-fields terminated by ',' 
-lines terminated by '\n'
-ignore 1 rows 
-(name,surname,@date_of_birth,id_document_number,id_document_type,id_document_authority)
-set date_of_birth = str_to_date(@date_of_birth, '%yyyy-%mm-%dd');
-
