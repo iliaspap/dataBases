5a8797f59a451bcd2f75da8db84bd22aa7e3f9ad
iliaspap <iliaspapanikolaou999@gmail.com>
2021-06-11 12:38:43 +0300

    complete getChargeInfo.php + index2.php

--- webapp/getChargeInfo.php
+++ webapp/getChargeInfo.php
@@ -7,18 +7,20 @@ $conn = new mysqli(
   $database =  "Hotel"
 );
 
-if($_GET['q'] == ""){
-    echo "YES";
-}else{
-    echo "NO";
+//** Change ii to dd when ready
+if ($_GET['q1'] == "All") {
+  $sql = "SELECT * FROM Service_usage WHERE service_charge_datetime >= ? and service_charge_datetime <= ? and amount >= ? and amount <= ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("ssii",$_GET['q2'], $_GET['q3'], $_GET['q4'], $_GET['q5']);
+}
+else {
+  $sql = "SELECT * FROM Service_usage WHERE service_description = ? and service_charge_datetime >= ? and service_charge_datetime <= ? and amount >= ? and amount <= ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("sssii", $_GET['q1'], $_GET['q2'], $_GET['q3'], $_GET['q4'], $_GET['q5']);
 }
 
-$str = "service_description = ?";
-$sql = "SELECT * FROM Service_usage WHERE ".$str;
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $_GET['q']);
 $stmt->execute();
-// $stmt->store_result();
+echo "Prossesing...";
 
 echo "<table>";
 echo "<tr>";
@@ -31,6 +33,10 @@ echo "<th>amount</th>";
 echo "</tr>";
 
 $result = $stmt->get_result();
+echo "Done";
+echo \n
+echo "Number of results: " ;
+echo $result->num_rows;
 
 if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
--- webapp/index.php
+++ webapp/index.php
@@ -13,7 +13,6 @@
 	if ($conn->connect_error) {
 	  die("Connection failed: " . $conn->connect_error);
 	}
-	echo "Connected successfully";
 ?>
 
 <html>
@@ -21,55 +20,84 @@
 		<meta charset="utf-8">
 		<title>The Grand Budapest Hotel</title>
 
-		<script type='text/javascript'>
-			function myFunction()
-			{
-				var x1 = document.getElementById("choose_service").value;
-				var x2 = document.getElementById("from_date").value;
-				var x3 = document.getElementById("to_date").value;
-				var x4 = document.getElementById("from_amount").value;
-				var x5 = document.getElementById("to_amount").value;
+		<!-- <button onclick="gotoCustomers()">Customers</button> -->
+		<a href="./index2.php">Service Usage</a>
 
-				// if (x2 < "2021-01-01" || x2 > "2021-12-31") {
-				// 	document.getElementById("Output").innerHTML = "Invalid or incomplete date";
-				// }
+		<script type='text/javascript'>
+
+			function askServiceUsage()
+			{
+				var q1;
+
+				/*default values for input*/
+				var q2 = "2000-01-01";//every date in the database is after this
+				var q3 = "2021-12-31";//everty date in the database is before this one
+				var q4 = 0; //every cost in the database is greater than this one
+				var q5 = 1000; //every cost in the database is less than this one
+
+				q1 = document.getElementById("choose_service").value;
+
+				if (document.getElementById("from_date").value != "") {
+					q2 = document.getElementById("from_date").value;
+					if(q2 < "1000-01-01" || q2>"10000-01-01") {
+						document.getElementById("Output").innerHTML = "Invalid date, try a year after 1000 and before 10000";
+						return
+					}
+
+				}
+				if (document.getElementById("to_date").value != "") {
+					q3 = document.getElementById("to_date").value;
+					if(q3 < "1000-01-01" || q2> "10000-01-01") {
+						document.getElementById("Output").innerHTML = "Invalid date, try something after 1000 and before 10000";
+						return
+					}
+				}
+
+				if(document.getElementById("from_amount").value != "") {
+					q4 = document.getElementById("from_amount").value;
+				}
+
+				if(document.getElementById("to_amount").value != "") {
+					q5 = document.getElementById("to_amount").value;
+				}
 
 				const xhttp = new XMLHttpRequest();
 				xhttp.onload = function() {
 					document.getElementById("Output").innerHTML = this.responseText;
 				}
 
-				xhttp.open("GET", "getChargeInfo.php?q="+x2);
+				xhttp.open("GET", "getChargeInfo.php?q1="+q1+"&q2="+q2+"&q3="+q3+"&q4="+q4+"&q5="+q5);
 				xhttp.send();
 			}
+
 		</script>
 
 	</head>
 
-	<body>
+	<body onload = "askServiceUsage()">
 		<form>
 			<label for="choose_service">Service Name</label>
-			<select id = "choose_service" name = "Service Type" onchange="myFunction()">
+			<select id = "choose_service" name = "Service Type" onchange="askServiceUsage()">
 				<option value = "All" selected>All</option>
 				<option value = "Drinks at the bar">Bar</option>
 				<option value = "Gym">Gym</option>
-				<option value = "Hairdresser">Hairdresser</option>
+				<option value = "Hairdressing">Hairdresser</option>
 				<option value = "Meeting Room">Meeting Room</option>
 				<option value = "Food and drinks at the restaurant">Restaurant</option>
 				<option value = "Sauna">Sauna</option>
 			</select>
 
 			<label for="From Date">From Date</label>
-			<input type="date" id="from_date" name="From Date" onchange="myFunction()">
+			<input type="date" id="from_date" name="From Date" onchange="askServiceUsage()">
 
 			<label for="To Date">To Date</label>
-			<input type="date" id="to_date" name="To Date" onchange="myFunction()">
+			<input type="date" id="to_date" name="To Date" onchange="askServiceUsage()">
 
 			<label for="From Amount">From Amount</label>
-			<input type="number" id="from_amount" name="From Amount" onchange="myFunction()">
+			<input type="number" id="from_amount" name="From Amount" onchange="askServiceUsage()">
 
 			<label for="To Amount">To Amount</label>
-			<input type="number" id="to_amount" name="To Amount" onchange="myFunction()">
+			<input type="number" id="to_amount" name="To Amount" onchange="askServiceUsage()">
 
 		</form>
 
