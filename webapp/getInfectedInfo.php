5ba214dbaea213dfa1e6ab57527ac127f8a9cbd0
Dionisis <dionisiskokiaris@gmail.com>
2021-06-14 18:44:55 +0300

    getInfected

--- webapp/getInfectedInfo.php
+++ webapp/getInfectedInfo.php
@@ -6,44 +6,30 @@ $conn = new mysqli(
 	$password = "databases2021",
 	$database = "asdf_palace"
 );
-//πρωτα βρίσκουμε τα visits του δοσμένου nfc_id
-//μετά κανουμε join με όλο τον πίνακα visits με συνθήκες τον χρόνο και το nfc_id διαφορετικό από το δοσμένου
-//από αυτό παίρνουμε μόνο το δεύτερο nfc_id pinf(possibly infected)
-//κάνουμε join με τον πίνακα των customers
-$sql = "WITH rightNFC AS (
-			SELECT *
-			FROM
-			WHERE nfc_id= ?),
-			pinf(nfc_id) AS (
-			SELECT v.nfc_id
-			FROM Visits as v
-			JOIN rightNFC as r
-			ON r.space_id=v.space_id AND r.nfc_id <> v.nfc_id AND  NOT (v.exit_datetime < r.arrival_datetime OR v.arrival_datetime > DATE_ADD(v.exit_datetime, INTERVAL 1 HOUR))
- 			)
-		SELECT *
-		FROM Customer
-		JOIN pinf ON pinf.nfc_id = Customer.nfc_id";
-
+ $sql = "SELECT DISTINCT c.nfc_id, c.name, c.surname, c.date_of_birth, c.id_document_number, c.id_document_type, c.id_document_authority
+ 				FROM Customer as c, Visits as v1, Visits as v2
+				WHERE v1.nfc_id= ? AND c.nfc_id=v2.nfc_id AND v1.nfc_id<>v2.nfc_id AND v1.space_id=v2.space_id AND ! ( v2.exit_datetime < v1.arrival_datetime OR v2.arrival_datetime > DATE_ADD(v1.exit_datetime, INTERVAL 1 HOUR) )";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $_GET['q']);
 $stmt->execute();
-
 $result = $stmt->get_result();
-echo "Number of visits: ";
+echo "Customers that might be infected: ";
 echo $result->num_rows;
 
 echo "<table>";
 echo "<tr>";
-echo "<th>space_id</th>";
-echo "<th>space_name</th>";
-echo "<th>space_description_location</th>";
-echo "<th>arrival_datetime</th>";
-echo "<th>exit_datetime</th>";
+echo "<th>nfc_id</th>";
+echo "<th>Name</th>";
+echo "<th>Surname</th>";
+echo "<th>Date of birth</th>";
+echo "<th>ID document number</th>";
+echo "<th>ID document type</th>";
+echo "<th>ID document authority</th>";
 echo "</tr>";
 
 if ($result->num_rows > 0) {
 	while ($row = $result->fetch_assoc()){
-		echo "<tr><td>".$row["space_id"]."</td><td> ".$row["name"]."</td><td> ".$row["description_location"]."</td><td> ".$row["arrival_datetime"]."</td><td> ".$row["exit_datetime"]."</td></tr>";
+		echo "<tr><td>".$row["nfc_id"]."</td><td> ".$row["name"]."</td><td> ".$row["surname"]."</td><td> ".$row["date_of_birth"]."</td><td> ".$row["id_document_number"]."</td><td>".$row["id_document_type"]."</td><td>".$row["id_document_authority"]."</td><td>";
 	}
 	echo "</table>";
 }
