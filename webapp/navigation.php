9ce88c03d327c3be2f94c184cac6c20130716a18
Dionisis <dionisiskokiaris@gmail.com>
2021-06-19 17:53:13 +0300

    11 erwthma

--- webapp/navigation.php
+++ webapp/navigation.php
@@ -1,4 +1,5 @@
 <a href="./index.php">Service Charges</a><!-- First page, answers question 7 -->
 <a href="./views.php">Views</a><!--answers question 8 -->
 <a href="./covid.php">Customer Visits</a><!-- answers question 9-->
-<a href="./infected.php">Possibly infected</a> <!-- answers question 10--> 
+<a href="./infected.php">Possibly infected</a> <!-- answers question 10-->
+<a href="./infoWrtAge.php">Possibly infected</a> <!-- answers question 11-->
