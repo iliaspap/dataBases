2a2d4eae4d037198bd14fe27c6f27bc0992675fa
Dionisis <dionisiskokiaris@gmail.com>
2021-06-19 17:55:44 +0300

    11 erwthma fix navigation

--- webapp/navigation.php
+++ webapp/navigation.php
@@ -2,4 +2,4 @@
 <a href="./views.php">Views</a><!--answers question 8 -->
 <a href="./covid.php">Customer Visits</a><!-- answers question 9-->
 <a href="./infected.php">Possibly infected</a> <!-- answers question 10-->
-<a href="./infoWrtAge.php">Possibly infected</a> <!-- answers question 11-->
+<a href="./infoWrtAge.php">Info by age</a> <!-- answers question 11-->
