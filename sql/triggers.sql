ad8dca52a021d2628a9fe0a8a3d9a50c0625820c
Marios Mantalos <marios.mad@gmail.com>
2021-06-19 20:48:24 +0300

    Nothing important.

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -3,6 +3,7 @@ drop trigger if exists grantAccess;
 create trigger grantAccess after insert on Registers
     for each row
     insert into HasAccess(nfc_id, start_datetime, space_id)
-    select distinct r.nfc_id, r.registration_datetime, of.space_id
-    from Registers as r, Offers as of
-    where r.service_id = NEW.service_id and r.nfc_id = NEW.nfc_id and r.service_id = of.service_id and NEW.service_id <> 7;
+    select distinct r.nfc_id, r.registration_datetime, o.space_id
+    from Registers as r, Offers as o
+    where r.service_id = NEW.service_id and r.nfc_id = NEW.nfc_id 
+        and r.service_id = o.service_id and NEW.service_id <> 7;
