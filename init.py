c12374e844422f56ff283f93bbb8a36b9543d597
Marios Mantalos <marios.mad@gmail.com>
2021-06-20 11:47:10 +0300

    Small output change.

--- init.py
+++ init.py
@@ -27,12 +27,12 @@ execute_script("./sql/hotel.sql")
 print("Done")
 
 # Create Triggers
-print("Create triggers...", end="")
+print("Creating triggers...", end="")
 execute_script("./sql/triggers.sql")
 print("Done")
 
 # Create Views
-print("Create views...", end="")
+print("Creating views...", end="")
 execute_script("./sql/views.sql")
 print("Done")
 
